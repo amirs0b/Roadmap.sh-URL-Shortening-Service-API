6e43d1b34b4a75845ac68dbdfdd1fe4f8b51f530
Amir Sobhani <amirsobhani@yahoo.com>
2025-09-13 20:44:14 +0330

    initialize project with basic structure and configuration files

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import {fileURLToPath} from "url";
+import path from "path";
+import morgan from "morgan";
+import {catchError, HandleERROR} from "vanta-api";
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.static('Public'))
+
+
+
+
+
+app.use((res, req, next) => {
+    return next(new HandleERROR("Not found", 404))
+})
+app.use(catchError)
+export default app;
